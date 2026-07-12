fae1dffe842b204640ece6efb072111234be3954
reboot0707 <bootcamp@contact.clam.tw>
2026-07-13 01:52:01 +0800

    refactor(SongQuery): 抽取查詢方法到專屬類別.

--- prjFruitBar8000/FrmSongQuery.cs
+++ prjFruitBar8000/FrmSongQuery.cs
@@ -1,9 +1,11 @@
 ﻿using prjFruitBar8000.DataEDMX;
+using prjFruitBar8000.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,130 +28,43 @@ namespace prjFruitBar8000
 
         private void btnQuery_Click(object sender, EventArgs e)
         {
-            bool flowControl = false;
-            //cbQueryType
-
-            if(cbQueryType.Text == "樂曲")
+            try
             {
-                try
-                { 
-                    flowControl = QuerySongs(); 
-                }
-                catch(Exception ex)
+                switch (cbQueryType.Text)
                 {
-                    ErrorLogger.Log(ex);
-                    MessageBox.Show("發生錯誤, 請洽系統管理員翻閱錯誤追蹤紀錄!");
-                    return;
+                    case "樂曲":
+                        CallQuerySongs();
+                        break;
+
+                    case "專輯":
+                    case "創作者":
+                        MessageBox.Show("功能正在實作中!");
+                        break;
                 }
             }
-
-            if (cbQueryType.Text == "專輯")
+            catch (Exception ex)
             {
-                try
-                {
-                    //flowControl = QuerySongs();
-                    MessageBox.Show("功能正在實作中!");
-                    return;
-                }
-                catch (Exception ex)
-                {
-                    ErrorLogger.Log(ex);
-                    MessageBox.Show("發生錯誤, 請洽系統管理員翻閱錯誤追蹤紀錄!");
-                    return;
-                }
-            }
-
-            if (cbQueryType.Text == "創作者")
-            {
-                try
-                {
-                    //flowControl = QuerySongs();
-                    MessageBox.Show("功能正在實作中!");
-                    return;
-                }
-                catch (Exception ex)
-                {
-                    ErrorLogger.Log(ex);
-                    MessageBox.Show("發生錯誤, 請洽系統管理員翻閱錯誤追蹤紀錄!");
-                    return;
-                }
-            }
-
-            if (!flowControl)
-            {
-                return;
+                ErrorLogger.Show(ex);
             }
         }
 
-        private bool QuerySongs()
+        public void CallQuerySongs()
         {
-            dbcontext = new FruitBarDBEntities();
+            string keyword = this.txtSongName.Text.Trim();
+            bool hasKeyword = keyword.Length > 0 && keyword != defaultTextboxName;
 
-            //TODO 例外處理
-            if (this.txtSongName.Text == defaultTextboxName || this.txtSongName.Text.Length == 0)
+            if (!hasKeyword)
             {
-                var qRawList = dbcontext.SongsAlbums
-                    .Where(x => !x.Song.IsDeleted)
-                    .SelectMany(
-                    a => a.Song.ArtistsSongs,
-                    (x, a) => new
-                    {
-                        SongName = a.Song.SongName,
-                        AlbumName = x.Album.AlbumName,
-                        ArtistName = a.Artist.ArtistName
-                    }
-                ).ToList();
-                var qParseList = qRawList.GroupBy(x => new
-                {
-                    x.SongName,
-                    x.AlbumName
-                })
-                    .Select(g => new
-                    {
-                        g.Key.SongName,
-                        g.Key.AlbumName,
-                        ArtistNames = string.Join("; ", g.Select(y => y.ArtistName))
-                    })
-                    .ToList();
-                dgvSongList.DataSource = qParseList.ToList();
-                dgvSongList.Columns["SongName"].HeaderText = "歌曲名稱";
-                dgvSongList.Columns["AlbumName"].HeaderText = "專輯名稱";
-                dgvSongList.Columns["ArtistNames"].HeaderText = "演出／製作人員";
-                return false;
-            }
-            else
-            {
-                string strSongQuery = txtSongName.Text.Trim();
-                var qRawList = dbcontext.SongsAlbums
-                    .Where(x => !x.Song.IsDeleted && x.Song.SongName.Contains(strSongQuery))
-                    .SelectMany(
-        a => a.Song.ArtistsSongs,
-        (x, a) => new
-        {
-            SongName = a.Song.SongName,
-            AlbumName = x.Album.AlbumName,
-            ArtistName = a.Artist.ArtistName
-        }
-    ).ToList();
-                var qParseList = qRawList.GroupBy(x => new
-                {
-                    x.SongName,
-                    x.AlbumName
-                })
-                    .Select(g => new
-                    {
-                        g.Key.SongName,
-                        g.Key.AlbumName,
-                        ArtistNames = string.Join("; ", g.Select(y => y.ArtistName))
-                    })
-                    .ToList();
-                dgvSongList.DataSource = qParseList.ToList();
-                dgvSongList.Columns["SongName"].HeaderText = "歌曲名稱";
-                dgvSongList.Columns["AlbumName"].HeaderText = "專輯名稱";
-                dgvSongList.Columns["ArtistNames"].HeaderText = "演出／製作人員";
+                keyword = "";
             }
 
-            return true;
+            List<SongQueryResult> qParseList = new SongQueryService().QuerySongs(keyword);
+
+            dgvSongList.DataSource = qParseList.ToList();
+            dgvSongList.Columns["SongName"].HeaderText = "歌曲名稱";
+            dgvSongList.Columns["AlbumName"].HeaderText = "專輯名稱";
+            dgvSongList.Columns["ArtistName"].HeaderText = "演出／製作人員";
+            return;
         }
 
         private void txtSongName_KeyDown(object sender, KeyEventArgs e)
---
+++ prjFruitBar8000/Services/SongQueryService.cs
@@ -0,0 +1,53 @@
+﻿using prjFruitBar8000.DataEDMX;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjFruitBar8000.Services
+{
+    public class SongQueryResult
+    {
+        public string SongName { get; set; }
+        public string AlbumName { get; set; }
+        public string ArtistName { get; set; }
+    }
+
+    public class SongQueryService
+    {
+        public List<SongQueryResult> QuerySongs(string inputKeyword)
+        {
+            using (FruitBarDBEntities dbcontext = new FruitBarDBEntities())
+            {
+                var query = dbcontext.SongsAlbums.Where(x => !x.Song.IsDeleted); //排除被軟刪除的資料
+                if (!string.IsNullOrWhiteSpace(inputKeyword))
+                {
+                    query = query.Where(x => x.Song.SongName.Contains(inputKeyword)); //查詢條件掛上要找關鍵字的情況
+                }
+
+                var qRawList = query.SelectMany(a => a.Song.ArtistsSongs,
+                                               (x, a) => new 
+                                               {
+                                                   a.Song.SongName,
+                                                   x.Album.AlbumName,
+                                                   a.Artist.ArtistName
+                                               }).ToList();  // 加上 ToList() 讓它從 LINQ to Entity 查詢語句轉換成可被 C# 處理(例如字串處理)的"具體"資料型態
+
+                var qParseList = qRawList
+                    .GroupBy(x => new { x.SongName, x.AlbumName })
+                    .Select(g => new SongQueryResult
+                    {
+                        SongName = g.Key.SongName,
+                        AlbumName = g.Key.AlbumName,
+                        ArtistName = string.Join("; ", g.Select(y => y.ArtistName))
+                    }).ToList();
+                return qParseList;
+            }
+        }     
+    }
+
+
+
+}
